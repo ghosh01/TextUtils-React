53e11588aa957d842a6aba920b1c2ece6fb438ab
Rohit Kumar Ghosh <100507672+ghosh01@users.noreply.github.com>
2023-04-11 08:22:00 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+const a="Rohit";
+const b="Rahul";
+const c="Ram";
+const d="Shyam";
+export default a;
+export {b};
+export {c};
+export {d};
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import x,{b,c,d} from './module1.mjs'
+console.log(x);
+console.log(b);
+console.log(c);
+console.log(d);
